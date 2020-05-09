8e048faf1419aff27cb18b7e128ed3f2905c00f8
james miller <jamesstewartmiller@gmail.com>
2020-05-09 15:47:11 +0100

    added a build so I can push to make github page

---
+++ build/precache-manifest.c76d70f137503efa77be95dcb31c2a17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f912a82743466438f04a239e7c8ccd6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb91b6d4cdf6aad8db3",
+    "url": "/static/css/main.b374fc42.chunk.css"
+  },
+  {
+    "revision": "19807314c2f0664925f3",
+    "url": "/static/js/2.d0252c34.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/static/js/2.d0252c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb91b6d4cdf6aad8db3",
+    "url": "/static/js/main.34235338.chunk.js"
+  },
+  {
+    "revision": "2da420af1caacc344726",
+    "url": "/static/js/runtime-main.a106bb16.js"
+  }
+]);
